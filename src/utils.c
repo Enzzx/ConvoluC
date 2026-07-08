d00c234212f637594a357a506580c2f1e90d9bf1
Enzzx <bellottie815@gmail.com>
2026-07-07 23:38:27 -0300

    deleting stb files since it's no longer necessary

--- src/utils.c
+++ src/utils.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 #include "../include/utils.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "../include/stb_image.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../include/stb_image_write.h"
 
 
 void swapImgRef(ImgH* handler, unsigned char* newData, int posterior) {
