b882c50366ed4a201c1be9eab4e90a13773e6439
Enzzx <bellottie815@gmail.com>
2026-09-23 02:47:30 -0300

    get file from input

--- src/main.c
+++ src/main.c
@@ -14,12 +14,15 @@
 #include "../include/types.h"
 #include "../include/clio.h"
 
-#define PATH "assets"
-#define FILE "pasaro"
+#define REFPATH "assets"
 
 int main() {
     char imgPath[256];
-    sprintf(imgPath, "%s/%s.png", PATH, FILE);
+    char filename[128];
+    printf("Digite o nome do arquivo: (path base -> '/%s')\n", REFPATH);
+    scanf("%s", filename);
+    sprintf(imgPath, "%s/%s", REFPATH, filename);
+
     MatrixH Convo;
     ImgH Image;
     configMatrix(&Image, &Convo);   // escolha do filtro
@@ -43,7 +46,7 @@ int main() {
     printf("tempo de execução: %f\n", tempo_execucao);
 
 
-    sprintf(imgPath, "%s-saida.png", FILE);
+    sprintf(imgPath, "%s-saida.png", filename);
     stbi_write_png(imgPath, Image.w, Image.h, Image.c, Image.data, Image.w * Image.c);  // salva saída
     printf("Imagem criada com sucesso\n");
 
